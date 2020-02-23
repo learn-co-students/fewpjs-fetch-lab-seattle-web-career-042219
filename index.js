8630f7e4c30064019b44ee24156f4d5773efebde
austenhasty <hastyae@gmail.com>
2020-02-23 01:13:28 -0800

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let url = 'https://anapioficeandfire.com/api/books'
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
 
 function fetchBooks() {
-   return fetch(url)
-  .then(resp => resp.json())
-  .then(json =>renderBooks(json))
+  let url = 'https://anapioficeandfire.com/api/books';
+    return fetch(url)
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 };
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 };
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-});
